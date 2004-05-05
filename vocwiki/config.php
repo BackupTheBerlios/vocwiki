8af030e7e9c3d28ed390091674c399078c6c6581
bolsog <bolsog>
2004-05-05 21:36:06 +0000

    gpl message added

--- vocwiki/config.php
+++ vocwiki/config.php
@@ -1,8 +1,26 @@
 <?php
 
+/*
+    Copyright (C) 2004 Andreas Madsack
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 // configuration for vocwiki
 //
-// $Id: config.php,v 1.2 2004/05/05 18:59:36 bolsog Exp $	
+// $Id: config.php,v 1.3 2004/05/05 21:36:06 bolsog Exp $	
 
 
 //
@@ -21,7 +39,7 @@ include("dbpw.php");
 ///////////////////////////////////////////////
 
 
-// enabling locale ... only php 4.1+ ?
+// enabling locale ... only php 4.1+ ?  (berlios is php4?)
 
 // todo: save LANG in usersettings!!!
 
